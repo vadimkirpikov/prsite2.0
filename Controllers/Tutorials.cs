7c97ecbcf722a18bd94458922ac32a1f9e484e0c
VadimKirpikov <vadim.kir13@rambler.ru>
2024-06-14 15:39:03 +0400

    full

--- Controllers/Tutorials.cs
+++ Controllers/Tutorials.cs
@@ -8,32 +8,35 @@ public class Tutorials : Controller
 {
     private ProgrammSiteContext db;
     private readonly IMemoryCache _cache;
-
     public Tutorials(ProgrammSiteContext context, IMemoryCache cache)
     {
         db = context;
         _cache = cache;
     }
-
     [Route("{lang}/{section}/{shortname}")]
-    public IActionResult About(string lang, string section, string shortname)
+    [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Client, VaryByQueryKeys = new[] { "lang", "section", "shortname" })]
+    public IActionResult About(string lang,string section, string shortname)
     {
 
         var url = $"/{lang}/{section}/{shortname}";
-        var sectionUrl = $"/{lang}/{section}";
-        
-        var tutorials = GetTutorialsFromCache(sectionUrl);
-        var allSections = GetSectionsFromCache();
-        var chaptersMap = GetChapters(allSections.First(s => s.Url.Equals(sectionUrl)).Id);
+        Console.WriteLine("Uncached in browser " + url);
+        var tutorials = GetTutorialsFromCache($"/{lang}/{section}");
+        var sectionId = db.Sections.First(s => s.Url.Equals($"/{lang}/{section}")).Id;
+        if (!_cache.TryGetValue(sectionId, out Dictionary<int, string> chaptersMap))
+        {
+            var chapters = db.Chapters.Where(ch => ch.SectionId.Equals(sectionId));
+            chaptersMap = new();
+            foreach (var chapter in chapters)
+                chaptersMap[chapter.Id] = chapter.Title;
+            _cache.Set(sectionId, chaptersMap, TimeSpan.FromMinutes(30));
+        }
         var tutorial = tutorials.First(t => t.Url.Equals(url));
-        
         var indexOfTutorial = tutorials.IndexOf(tutorial);
         var nextTutorialUrl = indexOfTutorial < tutorials.Count - 1 ? tutorials[indexOfTutorial + 1].Url : "invalid";
         var previousTutorialUrl = indexOfTutorial > 0 ? tutorials[indexOfTutorial - 1].Url : "invalid";
 
-        
         ViewBag.Chapters = chaptersMap;
-        ViewBag.Sections = allSections;
+        ViewBag.Sections = db.Sections.ToList();
         ViewBag.Title = tutorial.Title;
         ViewBag.Tutorial = tutorial;
         ViewBag.Tutorials = tutorials;
@@ -42,97 +45,58 @@ public class Tutorials : Controller
         ViewBag.Description = tutorial.Description;
         ViewBag.KeyWords = tutorial.Keywords;
         ViewBag.IsTutorial = true;
-        
         return View();
     }
 
     [Route("{lang}")]
     public IActionResult Main(string lang)
     {
-        var langs = GetLangs();
-        var Lang = langs.First(l => l.Name.Equals(lang));
-        var allSections = GetSectionsFromCache();
-        var sections = GetSectionsFromLang(Lang.Id);
+        var Lang = db.Langs.First(l => l.Name.Equals(lang));
         
-        ViewBag.Sections = allSections;
+        ViewBag.Tutorials = db.Sections.ToList();
+        ViewBag.Sections = db.Sections.ToList();
         ViewBag.Title = Lang.Title;
         ViewBag.IsTutorial = false;
-        ViewBag.Tutorials = sections;
+        if (!_cache.TryGetValue(Lang.Id, out IQueryable<Section>? sections))
+        {
+            sections = db.Sections.Where(s=>s.LangId==Lang.Id);
+            _cache.Set(Lang.Id, sections, TimeSpan.FromMinutes(30));
+        }
+        
         return View(sections);
     }
 
     [Route("")]
     public IActionResult Main()
     {
-        var allLangs = GetLangs();
-        var allSections = GetSectionsFromCache();
-        ViewBag.Sections = allSections;
-        ViewBag.Tutorials = allLangs;
+        ViewBag.Sections = db.Sections.ToList();
+        ViewBag.Tutorials = db.Langs.ToList();
         ViewBag.Title = "PROGA.RU";
         ViewBag.IsTutorial = false;
-        return View(allLangs);
+        
+        IEnumerable<IDescription> langs = db.Langs.ToList();
+        return View(langs);
     }
 
     [Route("{lang}/{section}")]
     public IActionResult Main(string lang, string section)
     {
+
         var tutorials = GetTutorialsFromCache($"/{lang}/{section}");
         var firstTutorial = tutorials[0];
+        
         return Redirect(firstTutorial.Url);
     }
     
-    private List<Tutorial>? GetTutorialsFromCache(string url)
+    [NonAction]
+    private List<Tutorial> GetTutorialsFromCache(string url)
     {
         if (!_cache.TryGetValue(url, out List<Tutorial>? tutorials))
         {
-            Console.WriteLine("Uncached url " + url);
+            Console.WriteLine("Uncached url "+url);
             tutorials = db.Tutorials.Where(t => t.Url.Contains(url)).ToList();
             _cache.Set(url, tutorials, TimeSpan.FromMinutes(30));
         }
         return tutorials;
-    }
-
-    private List<Section>? GetSectionsFromCache()
-    {
-        if (!_cache.TryGetValue("all_sections", out List<Section>? allSections))
-        {
-            allSections = db.Sections.ToList();
-            _cache.Set("all_sections", allSections, TimeSpan.FromMinutes(30));
-        }
-        return allSections;
-    }
-
-    private List<Section>? GetSectionsFromLang(int langId) 
-    {
-        if (!_cache.TryGetValue(langId, out List<Section>? sections))
-        {
-            sections = db.Sections.Where(s => s.LangId.Equals(langId)).ToList();
-            _cache.Set(langId, sections, TimeSpan.FromMinutes(30));
-        }
-        return sections;
-    }
-
-    private List<Lang>? GetLangs()
-    {
-        if (!_cache.TryGetValue("all_langs", out List<Lang>? langs))
-        {
-            langs = db.Langs.ToList();
-            _cache.Set("all_langs", langs, TimeSpan.FromMinutes(30));
-        }
-        return langs;
-    }
-
-    private Dictionary<int, string>? GetChapters(int sectionId)
-    {
-        if (!_cache.TryGetValue(sectionId, out Dictionary<int, string>? chaptersMap))
-        {
-            var chapters = db.Chapters.Where(ch => ch.SectionId.Equals(sectionId));
-            chaptersMap = new();
-            foreach (var chapter in chapters)
-                chaptersMap[chapter.Id] = chapter.Title;
-            _cache.Set(sectionId, chaptersMap, TimeSpan.FromMinutes(30));
-        }
-
-        return chaptersMap;
     }
 }
